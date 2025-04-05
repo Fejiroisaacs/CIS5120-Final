ff09a87d52e1da6f8f2e9bdeea547a3f1c7babb1
Jake Garrison Murphy <133890754+PhoebusEtBacchus@users.noreply.github.com>
2025-04-05 16:16:54 -0400

    new file:   .DS_Store 	modified:   music-app/src/App.js 	new file:   music-app/src/components/ProjectCard.css 	new file:   music-app/src/components/ProjectCard.js 	new file:   package-lock.json

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,9 +1,17 @@
 import BaseLayout from "./components/BaseLayout";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
   return (
     <BaseLayout>
       <h1>Music App 🎶</h1>
+      <div style={{ padding: "2rem" }}>
+      <ProjectCard
+        title="DISCO PRINCE"
+        members={["Jake Murphy", "Fejiro Anigboro", "Solomon Graf"]}
+        tags={["Pop", "Indie"]}
+      />
+    </div>
     </BaseLayout>
   );
 }
